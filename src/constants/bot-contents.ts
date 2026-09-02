896d32e5ad031858bb5a2724f0bb4375ac032bfc
Nesh1309 <bravinmunene73@gmail.com>
2026-09-02 20:44:08 +0300

    Update bot-contents.ts
    Add new tab structure

--- src/constants/bot-contents.ts
+++ src/constants/bot-contents.ts
@@ -15,11 +15,28 @@ export const DBOT_TABS: TDashboardTabIndex = Object.freeze({
     DASHBOARD: 0,
     BOT_BUILDER: 1,
     CHART: 2,
-    TUTORIAL: 3,
+    TRADING_BOTS: 3,
+    ANALYSIS_TOOLS: 4,
+    DTRADER: 5,
+    COPY_TRADING: 6,
+    TRADING_VIEW: 7,
+    RISK_CALCULATOR: 8,
+    TUTORIAL: 9,
 });
 
 export const MAX_STRATEGIES = 10;
 
-export const TAB_IDS = ['id-dbot-dashboard', 'id-bot-builder', 'id-charts', 'id-tutorials'];
+export const TAB_IDS = [
+    'id-dbot-dashboard',
+    'id-bot-builder',
+    'id-charts',
+    'id-trading-bots',
+    'id-analysis-tools',
+    'id-dtrader',
+    'id-copy-trading',
+    'id-trading-view',
+    'id-risk-calculator',
+    'id-tutorials',
+];
 
 export const DEBOUNCE_INTERVAL_TIME = 500;
